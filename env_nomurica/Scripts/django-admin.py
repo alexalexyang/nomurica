1e9dde71c0d95dfcf23f6965ed8245a52664e48e
alex yang <alexalexyang@gmail.com>
2018-03-01 00:19:36 +0100

    removed virtualenv

--- env_nomurica/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\alex\desktop\python\180220~1\nomurica\env_no~1\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
